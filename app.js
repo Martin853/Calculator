98f90eabfbdb4763a774f170f112a48c9707fa5f
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-02-04 22:09:47 +0100

    Created main variables

--- app.js
+++ app.js
@@ -13,3 +13,25 @@ let calculator = {
     return x / y;
   },
 };
+
+// Main Variables
+const resultText = document.querySelector(".text-box-paragraph");
+const ac = document.querySelector(".ac");
+const del = document.querySelector(".del");
+const plusMinus = document.querySelector(".plus-minus");
+const plus = document.querySelector(".plus");
+const minus = document.querySelector(".minus");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+const comma = document.querySelector(".comma");
+const equal = document.querySelector(".equal");
+const zero = document.querySelector(".zero");
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+const six = document.querySelector(".six");
+const seven = document.querySelector(".seven");
+const eight = document.querySelector(".eight");
+const nine = document.querySelector(".nine");
