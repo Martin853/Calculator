2acde27d31d146c37c444b61131b737b453813c5
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-02-07 18:49:13 +0100

    Rewrote the project

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
 // Main Variables
 const resultText = document.querySelector(".text-box-paragraph");
-const ac = document.querySelector(".ac");
-const del = document.querySelector(".del");
-const plus = document.querySelector(".plus");
-const minus = document.querySelector(".minus");
-const multiply = document.querySelector(".multiply");
-const divide = document.querySelector(".divide");
-const comma = document.querySelector(".comma");
-const equal = document.querySelector(".equal");
-const zero = document.querySelector(".zero");
-const one = document.querySelector(".one");
-const two = document.querySelector(".two");
-const three = document.querySelector(".three");
-const four = document.querySelector(".four");
-const five = document.querySelector(".five");
-const six = document.querySelector(".six");
-const seven = document.querySelector(".seven");
-const eight = document.querySelector(".eight");
-const nine = document.querySelector(".nine");
+const numbers = document.querySelectorAll(".calculator-number-button");
+
+console.log(numbers);
