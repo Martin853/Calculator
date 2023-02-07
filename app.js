5711e5d68fb8e6eb68b080ee03b16692ff687ec2
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-02-07 19:01:56 +0100

    Added main variables

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 // Main Variables
 const resultText = document.querySelector(".text-box-paragraph");
 const numbers = document.querySelectorAll(".calculator-number-button");
+const functions = document.querySelectorAll(".calculator-functional-button");
+const equal = document.querySelector(".equal");
 
-console.log(numbers);
+// Calculator Functions
+const calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  substract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+};
+
+// Calculator Variables
+let oldValue = "";
+let currentValue = "";
+let operator;
