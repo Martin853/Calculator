55ab5be1b21acd4713a22240efcc61e96204acab
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-02-07 20:05:17 +0100

    Finished buttons

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // Main Variables
 const resultText = document.querySelector(".text-box-paragraph");
 const numbers = document.querySelectorAll(".calculator-number-button");
-const functions = document.querySelectorAll(".calculator-functional-button");
+const calculatorFunctions = document.querySelectorAll(
+  ".calculator-functional-button"
+);
+const zero = document.querySelector(".zero");
 const equal = document.querySelector(".equal");
+const ac = document.querySelector(".ac");
 
 // Calculator Functions
 const calculator = {
@@ -22,5 +26,40 @@ const calculator = {
 
 // Calculator Variables
 let oldValue = "";
-let currentValue = "";
+let currentValue = "0";
 let operator;
+
+// Calculator
+
+// Numbers
+numbers.forEach(function (button) {
+  button.addEventListener("click", function () {
+    if (currentValue.length < 12) {
+      if (currentValue[0] == "0") {
+        currentValue = button.innerHTML;
+        resultText.innerHTML = currentValue;
+      } else {
+        currentValue += button.innerHTML;
+        resultText.innerHTML = currentValue;
+      }
+    }
+  });
+});
+
+// Zero
+zero.addEventListener("click", function () {
+  if (currentValue.length < 12) {
+    if (currentValue[0] != "0") {
+      currentValue += "0";
+      resultText.innerHTML = currentValue;
+    }
+  }
+});
+
+// AC
+ac.addEventListener("click", function () {
+  oldValue = "";
+  currentValue = "0";
+  operator = null;
+  resultText.innerHTML = currentValue;
+});
